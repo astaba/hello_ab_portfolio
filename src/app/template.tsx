e32bfd7ab5bdbfd50eaf4bd78628c93848b88700
Astar <bahouidi99@gmail.com>
2023-12-25 20:25:10 +0100

    Smooth page transition

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,14 +1,27 @@
 "use client";
 
 import React from "react";
+import { motion } from "framer-motion";
 
 import useScrollRate from "@/hooks/useScrollRate";
+
+const variants = {
+  hidden: { opacity: 0 },
+  enter: { opacity: 1 },
+};
 
 const Template = ({ children }: { children: React.ReactNode }) => {
   const completion = useScrollRate();
   return (
     <>
-      <main>{children}</main>
+      <motion.main
+        variants={variants}
+        initial="hidden"
+        animate="enter"
+        transition={{ type: "linear", delay: 0.2, duration: 0.4 }}
+      >
+        {children}
+      </motion.main>
       <span
         style={{ transform: `translateY(${completion - 100}%)` }}
         className="fixed bg-primary z-50 w-1 top-0 bottom-0 right-0 transition-all duration-300"
