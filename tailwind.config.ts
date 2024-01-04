e66e76346734b459394c78cabf74401cb08e19ab
Astar <bahouidi99@gmail.com>
2024-01-04 22:22:06 +0100

    Redefine screen queries breakpoints

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,12 @@ const config = {
     container: {
       center: true,
       padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
+    },
+    screens: {
+      sm: "64Opx",
+      md: "768px",
+      lg: "1024px",
+      xl: "1400px",
     },
     extend: {
       colors: {
