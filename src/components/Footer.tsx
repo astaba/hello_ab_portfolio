550761c1a796a44ed9a193e6715d8a36be978854
Astar <bahouidi99@gmail.com>
2024-01-12 21:25:21 +0100

    Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,21 @@
+import Socials from "./Socials";
+
+const socialStyles = {
+  wrapper: "mb-4 flex gap-x-6",
+  icons: "text-[20px] text-primary transition-all hover:text-white dark:text-white/70 dark:hover:text-primary",
+};
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bg-secondary py-12">
+      <div className="container">
+        <div className="flex flex-col items-center justify-between">
+          <Socials socialsStyles={socialStyles} />
+          <p className="text-muted-foreground">Copyright &copy; Astar Bahouidi. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
