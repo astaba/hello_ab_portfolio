bea7176d1673b1ecc5b4682665f146e9d79bd09f
Astar <bahouidi99@gmail.com>
2024-01-12 21:25:03 +0100

    Cta component

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -27,7 +27,7 @@ const Template = ({ children }: { children: React.ReactNode }) => {
         style={{ transform: `translateY(${completion - 100}%)` }}
         className="fixed bg-primary z-50 w-1 top-0 bottom-0 right-0 transition-all duration-300"
       ></span>
-      <div className="h-[4000px]"></div>
+      {/* <div className="h-[4000px]"></div> */}
     </>
   );
 };
